d0e6a6db7d16a4e7b0bed39420f64234eabc5244
Yurii Tyshchenko <orty222@gmail.com>
2024-01-09 17:18:28 +0100

    Initial Commit

---
+++ MathBubble/MathBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathBubbleApp.swift
+//  MathBubble
+//
+//  Created by student on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MathBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
