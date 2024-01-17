0032061b8bc8020102c4a5ce297b5fe3f4d92f5a
Yurii Tyshchenko <orty222@gmail.com>
2024-01-17 21:01:53 +0100

    final commit

--- MathBubble/DifficultyButton.swift
+++ MathBubble/DifficultyButton.swift
@@ -1,27 +1,18 @@
 //
-//  NumberPadButton.swift
+//  DifficultyButton.swift
 //  MathBubble
 //
-//  Created by student on 16/01/2024.
+//  Created by student on 17/01/2024.
 //
 
 import SwiftUI
 
 struct DifficultyButton: View {
-    
-    var title: String
-    var color: Color
-    var action: () -> Void
-    
     var body: some View {
-        Button(action: action, label: {
-            Text(title)
-                .font(.title)
-                .foregroundColor(.white)
-                .padding()
-                .background(color)
-                .cornerRadius(8.0)
-                .shadow(color: Color.black, radius: 3, x: 1, y: 1)
-        })
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
+
+#Preview {
+    DifficultyButton()
 }
